2cb1d77a9d997bd9617aed80343d968ea968d1e5
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2022-04-24 05:43:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+const routes = {
+    about: '/about',
+    services: '/services',
+    contact: '/contact',
+};
+
+const getH1 = text => {
+    return `<h1 style='text-align:center;margin-top:100px'>${text}</h1>`;
+};
+const server = http.createServer(({ url }, res) => {
+    let route = null;
+    if (url !== '/favicon.ico') {
+        console.log('server => url', url);
+        route = url.split('/')[1];
+    }
+    if (url === '/') {
+        res.end(getH1('Weclome to home pgae'));
+    } else if (url === routes[route]) {
+        res.end(getH1(`Weclome to ${route} pgae`));
+    } else {
+        res.end(getH1('This page is not found 404'));
+    }
+});
+
+const PORT = 8080;
+server.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+});
