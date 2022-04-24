95b8871eb147136a8c746d806dc455c913388349
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2022-04-24 06:20:20 +0200

    make it better

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const server = http.createServer(({ url }, res) => {
     }
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
 });
